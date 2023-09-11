f189fb77cb48ae15fe01a01481049aeeb276706e
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-09-11 15:27:11 -0300

    Operadores Aritmeticos

---
+++ Module 02/OperadoresAritmeticos.java
@@ -0,0 +1,28 @@
+public class OperadoresAritmeticos {
+
+  public static void main(String[] args) {
+    int age = 40;
+    int yourAge = 25;
+
+    // Addition
+    int totalSum = age + yourAge + 100;
+    System.out.println("Total sum value is: " + totalSum);
+
+    // Subtraction
+    int difference = age - yourAge;
+    System.out.println("Age difference is: " + difference);
+
+    // Multiplication
+    int multiplication = 2 * yourAge;
+    System.out.println("Multiplication is: " + multiplication);
+
+    // Division
+    int division = yourAge / 2;
+    System.out.println("Half your age is: " + division);
+
+    // Module
+    int restOfDivision = 7 % 2;
+    System.out.println("Rest of division is: " + restOfDivision);
+  }
+
+}
