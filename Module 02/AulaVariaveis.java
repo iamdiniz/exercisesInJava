85bc057fb797938bad3670ce6092ef0070d8cacd
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-09-11 15:01:05 -0300

    First commit

---
+++ Module 02/AulaVariaveis.java
@@ -0,0 +1,16 @@
+public class AulaVariaveis {
+  
+  public static void main(String[] args) {
+     // Não recomendado pela Code Convcentions da Oracle
+     // int minhaIdade = 40, suaIdade = 25;
+
+     // lowerCamelCase
+     int minhaIdade = 40;
+     int suaIdade = 25;
+     
+     int totalIdades = minhaIdade + suaIdade;
+     
+     System.out.println("Total das idades: " + totalIdades);
+  }
+
+}
