317656bed2b478e3f1bd812409b554fff0433286
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-09-13 14:33:18 -0300

    Conversão de tipos primitivos + Exercise

---
+++ Module 02/Exercise0216.java
@@ -0,0 +1,23 @@
+/*
+
+Um programador começou a desenvolver um programa para o departamento de
+logística de uma empresa, que calcula o peso total das cargas de um caminhão,
+porém ele saiu de férias e você foi chamado para finalizar o trabalho.
+Embora o peso das cargas seja com decimais (ponto-flutuante), o usuário quer
+saber o peso total das cargas sem casas decimais, ou seja, o valor total deve
+ser "truncado".
+Finalize o programa exibindo o peso total das cargas em inteiro.
+
+*/
+public class Exercise0216 {
+ 
+  public static void main(String[] args) {
+    double pesoCarga1 = 1033.49;
+    double pesoCarga2 = 849.88;
+
+    int pesoTotalCargas = (int) (pesoCarga1 + pesoCarga2);
+
+    System.out.println("Peso total das cargas: " + pesoTotalCargas);
+  }
+
+}
