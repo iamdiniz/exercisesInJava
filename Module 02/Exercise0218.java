1d44534c4722ebe4ab6330ea098f2ff2342e0941
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-09-14 12:44:17 -0300

    Promoção aritmetica + Exercise

---
+++ Module 02/Exercise0218.java
@@ -0,0 +1,13 @@
+public class Exercise0218 {
+ 
+  public static void main(String[] args) {
+    int idade1 = 30;
+    int idade2 = 40;
+    int idade3 = 60;
+    
+    float idadeMedia = (idade1 + idade2 + idade3) / 3f; // especificar com f para não perder precisão.
+
+    System.out.println("Idade media: " + idadeMedia);
+  }
+
+}
---
+++ Module 02/PromocaoAritmetica3.java
@@ -0,0 +1,11 @@
+public class PromocaoAritmetica3 {
+
+  public static void main(String[] args) {
+    int x = 3;
+    float y = 2;
+    float z = (float) x / y;
+
+    System.out.println(z);
+  }
+
+}
