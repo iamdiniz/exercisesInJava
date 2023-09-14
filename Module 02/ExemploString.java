96d6e7ab63882d0b73b58b24c481737305654d21
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-09-14 13:10:24 -0300

    Trabalhando com String

---
+++ Module 02/ExemploString.java
@@ -0,0 +1,17 @@
+public class ExemploString {
+
+  public static void main(String[] args) {
+    System.out.println("Fala, mergulhador!"); // Valor literal do tipo String
+
+    int x = 10;
+    int y = 5;
+
+    System.out.println("Resultado: " + (x + y));
+
+    String nome = "Maria";
+    int idade = 30;
+ 
+    System.out.println(nome + " tem " + idade + " anos");
+  }
+
+}
