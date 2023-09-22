9d7c495f5cab53371759144ef639ef6692e23ccb
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-09-22 15:28:30 -0300

    Switch Expressions

--- Module 03 - Control structures & Operators/HorarioFuncionamento.java
+++ Module 03 - Control structures & Operators/HorarioFuncionamento.java
@@ -7,27 +7,44 @@ public class HorarioFuncionamento {
 
     System.out.print("Digite um dia da semana (ex: seg, ter, qua, etc): ");
     String diaSemana = sc.nextLine();
+
+    System.out.print("Mes: ");
+    int mes = sc.nextInt();
+
+    // Switch expressions a partir do Java 14
+    /*
+    String horarioFuncionamento = switch (diaSemana) {
+      case "seg" -> "Fechado";
+      case "ter", "qua", "qui", "sex" -> "08:00 as 18:00";
+      case "sab", "dom" -> "08:00 as 12:00";
+      default -> "Dia invalido";
+    };
+    */
+
+    System.out.printf("Horario de funcionamento: %s%n", switch (diaSemana) {
+      case "seg" -> {
+        if (mes == 12) {
+          yield "08:00 as 16:00";
+        }
+	yield "Fechado";
+      }
+      case "ter", "qua", "qui", "sex" -> "08:00 as 18:00";
+      case "sab", "dom" -> "08:00 as 12:00";
+      default -> "Dia invalido";
+    });
+
+    /*
     String horarioFuncionamento;
 
     switch (diaSemana) {
-      case "seg":
-        horarioFuncionamento = "Fechado";
-        break;
-      case "ter":
-      case "qua":
-      case "qui":
-      case "sex":
-	horarioFuncionamento = "08:00 as 18:00";
-	break;
-      case "sab":
-      case "dom":
-        horarioFuncionamento = "08:00 as 12:00";
-        break;
-      default:
-        horarioFuncionamento = "Dia invalido";
+      case "seg" -> horarioFuncionamento = "Fechado";
+      case "ter", "qua", "qui", "sex" -> horarioFuncionamento = "08:00 as 18:00";
+      case "sab", "dom" -> horarioFuncionamento = "08:00 as 12:00";
+      default -> horarioFuncionamento = "Dia invalido";
     }
+    */
 
-    System.out.printf("Horario de funcionamento: %s%n", horarioFuncionamento);
+    // System.out.printf("Horario de funcionamento: %s%n", horarioFuncionamento);
   }
 
 }
